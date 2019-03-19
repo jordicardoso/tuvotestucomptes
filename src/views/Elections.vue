80426e536ea397efec7a046c800fbaf42b34f83f
Jordi Cardoso <jordi.cardoso@bonpreu.cat>
2019-03-19 12:10:17 +0100

    petits canvis variables

--- src/views/Elections.vue
+++ src/views/Elections.vue
@@ -6,10 +6,10 @@
           <v-icon color="primary" class="mr-2">{{ $route.meta.icon }}</v-icon>{{ $route.meta.title }}
         </v-card-title>
         <v-list two-line subheader>
-          <v-list-tile v-for="eleccio in eleccions" :key="eleccio.Desc" @click="openPartits(eleccio)">
+          <v-list-tile v-for="eleccio in eleccions" :key="eleccio.Descripció" @click="openPartits(eleccio)">
             <v-list-tile-content>
-              <v-list-tile-title class="text--primary">{{ eleccio.Desc }}</v-list-tile-title>
-                <v-list-tile-sub-title type="date">{{ eleccio.Data | getDate }}</v-list-tile-sub-title>
+              <v-list-tile-title class="text--primary">{{ eleccio.Descripció }}</v-list-tile-title>
+                <v-list-tile-sub-title type="date">{{ eleccio.data | getDate }}</v-list-tile-sub-title>
               </v-list-tile-content>
               <v-list-tile-action>
                 <v-btn icon ripple>
@@ -22,10 +22,10 @@
       <v-card color="blue-grey darken-2" class="white--text mt-3" v-if="eleccio">
         <v-card-title><h2>Partits</h2></v-card-title>
         <v-list two-line subheader>
-          <v-list-tile v-for="partit in partits" :key="partit.Nomcurt">
+          <v-list-tile v-for="partit in partits" :key="partit.abreviat">
             <v-list-tile-content>
-              <v-list-tile-action-text class="text--primary">{{ partit.Nomcurt }}</v-list-tile-action-text>
-                <v-list-tile-sub-title>{{ partit.Nom }}</v-list-tile-sub-title>
+              <v-list-tile-action-text class="text--primary">{{ partit.abreviat }}</v-list-tile-action-text>
+                <v-list-tile-sub-title>{{ partit.nom }}</v-list-tile-sub-title>
               </v-list-tile-content>
               <v-list-tile-action>
                 <v-btn icon ripple>
@@ -55,7 +55,7 @@ export default {
   },
   firestore () {
     return {
-      eleccions: db.collection('eleccions').orderBy('Data')
+      eleccions: db.collection('eleccions').orderBy('data')
     }
   },
   computed: {
@@ -64,7 +64,7 @@ export default {
     getDate: function (value) {
       const outDate = value
       let day = outDate.toDate().getDate()
-      let month = outDate.toDate().getMonth()
+      let month = outDate.toDate().getMonth() +1
       let year = outDate.toDate().getFullYear()
       return (day + '/' + month + '/' + year)
     }
@@ -74,7 +74,7 @@ export default {
       this.partits = []
       this.eleccio = eleccio
       var self = this
-      db.collection('eleccions').doc('K92HNeLF7hsBDA1KntKB').collection('partits').orderBy('Nomcurt')
+      db.collection('eleccions').doc('K92HNeLF7hsBDA1KntKB').collection('partits').orderBy('abreviat')
         .get()
         .then(function (querySnapshot) {
           querySnapshot.forEach(function (doc) {
